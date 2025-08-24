56bfd6a4e1d7257412a4b46ec706861cd3e1f0c9
HarshJajaniya <ajjujajaniya@gmail.com>
2025-08-24 14:28:41 +0530

    commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,10 +4,12 @@ import GitHubProvider from "next-auth/providers/github";
 const handler = NextAuth({
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.local.GITHUB_ID,
+      clientSecret: process.env.local.GITHUB_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
 });
 
 export { handler as GET, handler as POST };
--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,43 +1,33 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import { useSession, signOut, signIn } from "next-auth/react";
 
 const Navbar = () => {
   const { data: session } = useSession();
-
   if (session) {
     return (
-      <nav className="bg-blue-950 text-white flex justify-between px-4 h-16 items-center">
-        <div className="logo font-bold text-2xl">Get me a chai!</div>
-        <div>
-          Signed in as {session.user.email} <br />
-          <button
-            onClick={() => signOut()}
-            className="text-white bg-red-600 hover:bg-red-700 
-                       focus:ring-4 focus:outline-none focus:ring-red-300 
-                       font-medium rounded-lg text-sm px-5 py-2.5"
-          >
-            Sign out
-          </button>
-        </div>
-      </nav>
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
     );
   }
-
   return (
     <nav className="bg-blue-950 text-white flex justify-between px-4 h-16 items-center">
       <div className="logo font-bold text-2xl">Get me a chai!</div>
       <div>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl 
+        <Link href={"/login"}>
+          <button
+            type="button"
+            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl 
                      focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 
                      font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-          onClick={() => signIn("github")}
-        >
-          Login
-        </button>
+          >
+            Login
+          </button>
+        </Link>
       </div>
     </nav>
   );
