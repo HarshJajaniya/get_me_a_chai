b231ec94e9343c7666780ae164805d75d5c9476f
HarshJajaniya <ajjujajaniya@gmail.com>
2025-08-31 13:49:51 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXTAUTH_URL: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}` // On Vercel → use deployed domain
+      : "http://localhost:3000", // Locally → use localhost
+  },
+};
 
 export default nextConfig;
